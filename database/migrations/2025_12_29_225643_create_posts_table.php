cc3d55e873873d18761481f2e3babc8cf29c2caf
Jared Barojas <jaredbarojas90@gmail.com>
2025-12-30 00:17:41 +0100

    removed linting test line

--- database/migrations/2025_12_29_225643_create_posts_table.php
+++ database/migrations/2025_12_29_225643_create_posts_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('title');
             $table->string('slug');
             $table->text('body');
-            $table->text('excerpt');
             $table->timestamps();
         });
     }
