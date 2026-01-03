c454c963e2ac48297d5f69c643fb237f8c2ad48c
Jared Barojas <jaredbarojas90@gmail.com>
2026-01-03 14:36:23 +0100

    add description column to posts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +12,5 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        User::factory()->create([
-            'name' => 'chipotle',
-            'email' => 'jaredbarojas90@gmail.com',
-        ]);
-    }
+    public function run(): void {}
 }
