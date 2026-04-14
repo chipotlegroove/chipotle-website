e056aaed33e0afdcf3e69b471640d14217c22e88
Jared Barojas <jaredbarojas90@gmail.com>
2026-04-13 21:33:04 -0700

    feat: add comment model and relationship

---
+++ app/Models/Comment.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
+
+class Comment extends Model
+{
+    use HasRecursiveRelationships;
+
+    protected $fillable = [
+        'email',
+        'body',
+        'post_id',
+        'parent_id',
+    ];
+
+    /** @return BelongsTo<Post, $this> */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
