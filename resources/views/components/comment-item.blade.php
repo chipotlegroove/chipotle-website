8ba72925d9224cf1ccb72738b1e6ccb13e563cf9
Jared Barojas <jaredbarojas90@gmail.com>
2026-04-14 18:55:13 -0700

    fix: corrected issue where scrolling to thread start would fail because of missing refs

--- resources/views/components/comment-item.blade.php
+++ resources/views/components/comment-item.blade.php
@@ -1,8 +1,9 @@
 @props(['comment', 'depth'])
 <div x-data="{
     open: false,
-}" @close-all-reply-forms.window = "open = false" style="margin-left: {{ $depth * 16 }}px"
-    class="border-b border-gray-300 pb-4" x-ref="comment{{ $comment->getKey() }}">
+    }"
+    @close-all-reply-forms.window = "open = false" style="margin-left: {{ $depth * 16 }}px"
+    class="border-b border-gray-300 pb-4" id="comment-{{ $comment->getKey() }}">
     <p class="font-bold text-lg">Anonymous</p>
     <x-clampable-text :text="$comment->body" />
     <div class="flex space-x-4 w-fit">
@@ -10,7 +11,7 @@
             @click="if (!open) { $dispatch('close-all-reply-forms'); open=true; }">Reply</button>
         @if ($comment->parent_id)
             <button class="text-blue-500 hover:text-blue-700 cursor-pointer transition-colors"
-                @click="$refs.comment{{ $comment->rootAncestor->getKey() }}.scrollIntoView({ behavior: 'smooth' })">
+                @click="document.getElementById('comment-{{ $comment->rootAncestor->getKey() }}').scrollIntoView({ behavior: 'smooth' })">
                 Start of thread
             </button>
         @endif
