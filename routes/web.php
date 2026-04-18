219c99c0c2c02fcd34ac1659bfc0f9005a0ca8ef
Jared Barojas <jaredbarojas90@gmail.com>
2026-04-18 16:26:29 -0700

    feat: implemented honeypot for comments and rate limiting

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,14 @@ use Spatie\Honeypot\ProtectAgainstSpam;
 
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
-Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store')->middleware(ProtectAgainstSpam::class);
-Route::post('/comments/{comment}/replies', [CommentController::class, 'storeReply'])->name('comments.store-reply');
+Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store')
+    ->middleware([
+        ProtectAgainstSpam::class,
+        'throttle:comments',
+    ]);
+Route::post('/comments/{comment}/replies', [CommentController::class, 'storeReply'])->name('comments.store-reply')
+    ->middleware([
+        'throttle:comments',
+    ]);
 Route::get('/tags', [TagController::class, 'index'])->name('tags.index');
 Route::get('/posts/tag/{tag:slug}', [PostTagController::class, 'show'])->name('posts-tags.show');
