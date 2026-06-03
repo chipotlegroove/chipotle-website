3d13c8e31b8d68dfc69d8f3a8ecb4729311182be
Jared Barojas <jaredbarojas90@gmail.com>
2026-06-03 12:04:43 -0700

    feat: added tracking to navbar links pretty

--- resources/views/components/navbar-link.blade.php
+++ resources/views/components/navbar-link.blade.php
@@ -1,8 +1,8 @@
-@props([
-    "active"
-])
+@props(['active'])
 
-<a {{ $attributes->class([
-    "inline-block text-white text-lg hover:scale-[1.15] hover:underline transition",
-    'font-bold' => $active
-]) }}> {{ $slot }}</a>
+<a
+    {{ $attributes->class([
+        'inline-block text-white text-lg hover:tracking-widest transition-all duration-300',
+        'font-bold' => $active,
+    ]) }}>
+    {{ $slot }}</a>
