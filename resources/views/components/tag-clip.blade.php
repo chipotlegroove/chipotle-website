ff953e9b09f7c9f6f60c81645659cf537f6a1882
Jared Barojas <jaredbarojas90@gmail.com>
2026-06-06 00:13:45 -0700

    feat: added nice hover transitions to tag clip

--- resources/views/components/tag-clip.blade.php
+++ resources/views/components/tag-clip.blade.php
@@ -2,6 +2,6 @@
     'label'
 ])
 <span
-    {{ $attributes->merge(['class' => 'cursor-pointer bg-sky-600 text-white text-sm px-2 py-1 rounded-xl hover:bg-sky-700 border']) }}>
+    {{ $attributes->merge(['class' => 'cursor-pointer bg-sky-600 text-white text-sm px-2 py-1 rounded-xl hover:bg-sky-700 hover:tracking-wide transition-all duration-300 border']) }}>
     {{ $label }}
 </span>
