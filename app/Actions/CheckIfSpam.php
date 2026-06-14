d5ae67214fd209a40637acac034f26d1d4da6098
Jared Barojas <jaredbarojas90@gmail.com>
2026-06-14 00:24:13 -0700

    epic linting thanks zed

--- app/Actions/CheckIfSpam.php
+++ app/Actions/CheckIfSpam.php
@@ -19,30 +19,39 @@ final class CheckIfSpam
     public function handle(AkismetContext $context, string $content): bool
     {
         try {
-            $response = $this->http->asForm()->post('https://rest.akismet.com/1.1/comment-check', [
-                'api_key' => config('services.akismet.key'),
-                'blog' => config('app.url'),
-                'user_ip' => $context->userIp,
-                'user_agent' => $context->userAgent,
-                'referrer' => $context->referrer,
-                'comment_type' => 'comment',
-                'comment_content' => $content,
-                'blog_lang' => 'en',
-                'blog_charset' => 'UTF-8',
-            ]);
+            $response = $this->http
+                ->asForm()
+                ->post('https://rest.akismet.com/1.1/comment-check', [
+                    'api_key' => config('services.akismet.key'),
+                    'blog' => config('app.url'),
+                    'user_ip' => $context->userIp,
+                    'user_agent' => $context->userAgent,
+                    'referrer' => $context->referrer,
+                    'comment_type' => 'comment',
+                    'comment_content' => $content,
+                    'blog_lang' => 'en',
+                    'blog_charset' => 'UTF-8',
+                ]);
         } catch (ConnectionException $e) {
-            $this->logger->warning('Akismet unreachable', ['error' => $e->getMessage()]);
+            $this->logger->warning('Akismet unreachable', [
+                'error' => $e->getMessage(),
+            ]);
 
             return false;
         }
 
         if ($response->failed()) {
-            $this->logger->warning('Akismet check failed', ['status' => $response->status()]);
+            $this->logger->warning('Akismet check failed', [
+                'status' => $response->status(),
+            ]);
 
             return false;
         }
 
-        $this->logger->info('Akismet response: ', ['body' => $response->body(), 'status' => $response->status()]);
+        $this->logger->info('Akismet response: ', [
+            'body' => $response->body(),
+            'status' => $response->status(),
+        ]);
 
         return $response->body() === 'true';
     }
